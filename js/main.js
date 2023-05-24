aa3b5419a8ea08f0b98c20214ea1b61c9815457b
JuanDanielG <jdgaleanon@ufpso.edu.co>
2023-05-23 21:30:00 -0500

    Modificaciones para el segundo parcial

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//declaramos la variable
+let comprar = document.querySelectorAll(".button")[0];
+
+//funcion del footer para el año del proyecto
+const año = document.getElementById('año-actual');
+año.innerHTML = new Date().getFullYear();
+
+
+// Obtén una referencia al botón de compra utilizando el id
+const botonCompra = document.getElementById('botonCompra');
+
+// Agrega un event listener para el evento 'click' al botón
+botonCompra.addEventListener('click', function() {
+   
+  // Por ejemplo, puedes mostrar un mensaje de confirmación
+  alert('¡Gracias por tu compra!');
+  // También puedes realizar otras acciones, como enviar datos al servidor, mostrar u ocultar elementos, etc.
+});
+
+document.getElementById("btnComprar").addEventListener("click", function comprar() {
+    window.location.href = "formulariodeCompra.html";
+  });
+  
