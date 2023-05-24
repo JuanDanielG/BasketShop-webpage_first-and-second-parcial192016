446d2ba0c128e2db98c8bf85c24d653e892c013f
JuanDanielG <jdgaleanon@ufpso.edu.co>
2023-05-23 23:37:28 -0500

    modificaciones de la pagina segundo parcial vrs3

--- js/main.js
+++ js/main.js
@@ -6,19 +6,75 @@ const año = document.getElementById('año-actual');
 año.innerHTML = new Date().getFullYear();
 
 
-// Obtén una referencia al botón de compra utilizando el id
+// BOTON DE COMPRA PARA EL SWEET
 const botonCompra = document.getElementById('botonCompra');
-
 // Agrega un event listener para el evento 'click' al botón
 botonCompra.addEventListener('click', function() {
    
   // Por ejemplo, puedes mostrar un mensaje de confirmación
-  alert('¡Gracias por tu compra!');
-  // También puedes realizar otras acciones, como enviar datos al servidor, mostrar u ocultar elementos, etc.
+  Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Gracias por tu compra!',
+    showConfirmButton: false,
+    timer: 1500
+  })
+
 });
 
 //Boton comprar en la tienda que te redirige al formulario de compra
-document.getElementById("btnComprar").addEventListener("click", function comprar() {
+botonCompra.addEventListener('click', function() {
+   
+  // Por ejemplo, puedes mostrar un mensaje de confirmación
+  Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Gracias por tu compra!',
+    showConfirmButton: false,
+    timer: 1500
+  })
+
+});
+document.getElementById('botonCompra').addEventListener("click", function comprar() {
     window.location.href = "formulariodeCompra.html";
   });
   
+  //
+  function enviar(){
+  Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Has completado tus datos!',
+    showConfirmButton: false,
+    timer: 1500
+  })}
+
+
+  
+  // Obtén una referencia al botón de envío
+const miBoton = document.getElementById('botonCompra');
+
+// Agrega un event listener al botón de envío
+miBoton.addEventListener('click', function(event) {
+  // Evita que el formulario se envíe de forma predeterminada
+  event.preventDefault();
+
+  // Muestra el Sweet Alert
+  Swal.fire({
+    title: '¡Éxito!',
+    text: 'El formulario se ha enviado correctamente.',
+    icon: 'success',
+    confirmButtonText: 'Aceptar'
+  }).then((result) => {
+    // Aquí puedes realizar alguna acción adicional después de cerrar el Sweet Alert
+    if (result.isConfirmed) {
+      // Por ejemplo, puedes enviar el formulario manualmente
+      document.getElementById('../formulariodeCompra.html').submit();
+    }
+  });
+});
+
+//BOTON EN EL FORMULARIO DE REGISTRO QUE ME ENVIE A LA PAGINA PRINCIPAL
+document.getElementById('botonCompra').addEventListener("click", function comprar() {
+  window.location.href = "../index.html";
+});
