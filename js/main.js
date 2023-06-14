7b6accc522de1e98a88bcabaac2f8d5280ca5d9f
JuanDanielG <jdgaleanon@ufpso.edu.co>
2023-06-14 12:23:16 -0500

    cambios de errores

--- js/main.js
+++ js/main.js
@@ -80,11 +80,11 @@ document.getElementById('#botonCompra').addEventListener("click", function compr
 });
 
 // Validaciones Formulario de compra
-const nombre = document.getElementById("name");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const form = document.getElementById("form");
-const parrafo = document.getElementById("warnings");
+const nombre = document.getElementById("#name");
+const email = document.getElementById("#email");
+const password = document.getElementById("#password");
+const form = document.getElementById("#form");
+const parrafo = document.getElementById("#warnings");
 
 form.addEventListener("submit", e=>{
   e.preventDefault() //para q
